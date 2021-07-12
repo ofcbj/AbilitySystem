16d30ee7ed1cb20912fccb43a6c8e5174d08be1e
ofcbj <hyojepapa@gmail.com>
2021-07-12 22:40:40 +0900

    anim bp 변경

---
+++ Source/AbilitySystem/Private/AnimInstanceBase.cpp
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "AnimInstanceBase.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "Gameframework/NavMovementComponent.h"
+
+void UAnimInstanceBase::AnimNotify_StartFullBody()
+{
+	ShouldDoFullBody = true;
+}
+
+void UAnimInstanceBase::AnimNotify_StartHalfBody()
+{
+	ShouldDoFullBody = false;
+}
+
+void UAnimInstanceBase::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+
+	auto OwnedCharacter = TryGetPawnOwner();
+	if (!::IsValid(OwnedCharacter)) return;
+
+	TArray<USkeletalMeshComponent*> MeshComp;
+	OwnedCharacter->GetComponents<USkeletalMeshComponent>(MeshComp);
+
+	USkeletalMeshComponent* Mesh = MeshComp[0];
+
+	FTransform TM = Mesh->K2_GetComponentToWorld();
+	FVector Velocity = OwnedCharacter->GetVelocity();
+
+	MeshSpaceVel = UKismetMathLibrary::InverseTransformDirection(TM, Velocity);
+	MeshSpaceVel.X *= -1.0;
+
+	TArray<UNavMovementComponent*> MoveComp;
+	OwnedCharacter->GetComponents<UNavMovementComponent>(MoveComp);
+	IsFalling = MoveComp[0]->IsFalling();
+}
---
+++ Source/AbilitySystem/Public/AnimInstanceBase.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "AnimInstanceBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ABILITYSYSTEM_API UAnimInstanceBase : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+private:
+	UFUNCTION()
+	void AnimNotify_StartFullBody();
+
+	UFUNCTION()
+	void AnimNotify_StartHalfBody();
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Pawn, Meta = (AllowPrivateAccess = true))
+	bool IsFalling;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Pawn, Meta = (AllowPrivateAccess = true))
+	bool ShouldDoFullBody;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Pawn, Meta = (AllowPrivateAccess = true))
+	FVector MeshSpaceVel;
+
+	void NativeUpdateAnimation(float DeltaSeconds) override;
+};
