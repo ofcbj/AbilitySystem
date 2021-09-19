261173fbc45bcb5e63a40e418910517f890123d2
ofcbj <hyojepapa@gmail.com>
2021-09-19 18:45:32 +0900

    테스트

--- Source/AbilitySystem/Public/CharacterBase.h
+++ Source/AbilitySystem/Public/CharacterBase.h
@@ -32,10 +32,10 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
-	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "CharacterBase")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "CharacterBase")
 	UAbilitySystemComponent* AbilitySystemComp;
 
-	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "CharacterBase")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "CharacterBase")
 	UAttributeSetBase* AttributeSetBaseComp;
 
 	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const;
